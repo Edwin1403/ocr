349295766c886f4dc989a0e8dfa154bfea5607e7
Edwin1403 <109071573+Edwin1403@users.noreply.github.com>
2022-12-02 15:41:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Components/Home'
+
+
+const App = () => {
+  return (
+    <div>
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Home/>} />
+  
+    </Routes>
+    </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/ImagePDF.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ImagePDF = () => {
+  return (
+    <div>ImagePDF</div>
+  )
+}
+
+export default ImagePDF
